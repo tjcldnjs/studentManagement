7f82a58bde2b91cfae1b14f2b038a1b6b456925b
lgw <spglados@gmail.com>
2024-06-28 12:45:31 +0900

    aa

---
+++ src/ver11111/DBConnectionManager.java
@@ -0,0 +1,46 @@
+package ver11111;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+// 싱글톤 패턴 - 단 하나의 객체만 필요함을 보장해야 된다면
+// 싱글톤 패턴으로 설계 할 수 있다.
+public class DBConnectionManager {
+
+	private static final String URL = "jdbc:mysql://192.168.0.140:3306/greencar?serverTimezone=Asia/Seoul";
+	private static final String USER = "tenco4";
+	private static final String PASSWORD = "1q2w3e4r5t!";
+
+	// 자기 자신의 참조 주소값을 담을 변수 생성 단, private
+	private static DBConnectionManager instance;
+	private HikariDataSource dataSource;
+
+	// 생성자를 <--- 외부에서 생성자를 호출 못하게 막아야 한다.
+	private DBConnectionManager() {
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl(URL);
+		config.setUsername(USER);
+		config.setPassword(PASSWORD);
+		config.setMaximumPoolSize(10);
+		dataSource = new HikariDataSource(config);
+	}
+
+	// 외부에서 클래스이름.getxxx 메서드를 만들어 주면 된다.
+	// synchronized 쓰는이유 - 한번에 하나의 스레드만 접근하도록 동기화 적용
+	public synchronized static DBConnectionManager getInstance() {
+		// 한번도 쓰지않았다면
+		if (instance == null) {
+			instance = new DBConnectionManager();
+		}
+		return instance;
+	}
+
+	// Connection 객체를 반환 (구현체 - HikariCP 이다)
+	public Connection getConnection() throws SQLException {
+		return dataSource.getConnection();
+	}
+
+}
---
+++ src/ver11111/StudentManagementSystem.java
@@ -0,0 +1,25 @@
+package ver11111;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import ver2.model.StudentDTO;
+
+public class StudentManagementSystem {
+
+	private static final StudentDAO studentDAO = new StudentDAO();
+
+	public static void main(String[] args) {
+		// 사용자에게 보여주는 부분 꾸며 줘도 됨
+
+		try {
+			List<StudentDTO> list = studentDAO.getAllStudents();
+			System.out.println(list.size());
+			System.out.println(list.toString());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
